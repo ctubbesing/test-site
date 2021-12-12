b5e7f0bb9b163c2c370cfeae218ce60e23eaddd5
Connor Tubbesing <connortubbesing@gmail.com>
2021-12-12 11:54:19 -0600

    Clean up icon things

--- fourth-try/src/views/About.vue
+++ fourth-try/src/views/About.vue
@@ -1,14 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <h1>asdfasdfasdf</h1>
+    <h1>Sample about page</h1>
     <img
       style="max-width: 512px"
       src="@/assets/icon.svg"
-    />
-    <img
-      style="max-width: 512px"
-      src="@/assets/icon2.svg"
     />
     <br>
     <snake-graphic
