78efa82499c9bf9865a544be75b9c1daed604a73
Connor Tubbesing <connortubbesing@gmail.com>
2021-12-25 07:11:57 -0600

    Try changing publicPath to get AWS to work

--- fourth-try/vue.config.js
+++ fourth-try/vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/test-site/" : "/"
+    publicPath: "/"
+    // publicPath: process.env.NODE_ENV === "production" ? "/test-site/" : "/"
 }
