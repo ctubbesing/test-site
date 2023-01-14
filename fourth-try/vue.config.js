d8a7dce14838183375f232bb79e8d935833339d9
Connor Tubbesing <connortubbesing@gmail.com>
2023-01-14 17:35:53 -0600

    Fix config

--- fourth-try/vue.config.js
+++ fourth-try/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    publicPath: process.env.BASE_ENV === "https://ctubbesing.github.io" ? "/test-site/" : "/"
+    // publicPath: process.env.BASE_ENV === "https://ctubbesing.github.io" ? "/test-site/" : "/"
     // publicPath: window.location.origin === "https://ctubbesing.github.io" ? "/test-site/" : "/"
     // publicPath: "/"
-    // publicPath: process.env.NODE_ENV === "production" ? "/test-site/" : "/"
+    publicPath: process.env.NODE_ENV === "production" ? "/test-site/" : "/"
 }
