eed4557ab7b0dda237e1d9ab3eeafaae7e58aa39
Connor Tubbesing <connortubbesing@gmail.com>
2024-08-31 15:55:14 -0500

    Update deploy scripts to target new project

---
+++ vue3-upgrade-test/scripts/gh-pages-deploy.mjs
@@ -0,0 +1,31 @@
+import {execa} from "execa";
+// import { execa } from "/node_modules/execa/index.js"
+import { existsSync } from "fs";
+
+(async () => {
+  try {
+    // make sure there are no uncommitted changes
+    await execa("git", ["update-index", "--refresh"]); 
+
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    console.log("Building...");
+    await execa("npm", ["run", "build"]);
+    // Understand if it's dist or build folder
+    const folderName = existsSync("dist") ? "dist" : "build";
+    await execa("git", ["--work-tree", folderName, "add", "--all"]);
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+    console.log("Pushing to gh-pages...");
+    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+    console.log("Cleaning up...")
+    await execa("rm", ["-r", folderName]);
+    await execa("git", ["checkout", "-f", "main"]);
+    await execa("git", ["branch", "-D", "gh-pages"]);
+    console.log("Successfully deployed");
+  } catch (e) {
+    if (e.cmd === 'git update-index --refresh') {
+      console.log('Please stash or commit changes first!')
+    }
+    console.log("Error: " + e.message);
+    process.exit(1);
+  }
+})();
