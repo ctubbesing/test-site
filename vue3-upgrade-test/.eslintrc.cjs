7f169e41397727306834c17f1557c5309ee6cbcc
Connor Tubbesing <connortubbesing@gmail.com>
2024-09-01 14:11:48 -0500

    Clean up deploy scripts and add mobile support to index.html

--- vue3-upgrade-test/.eslintrc.cjs
+++ vue3-upgrade-test/.eslintrc.cjs
@@ -3,7 +3,6 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  // node: true, ///////////// this fixes "process" not being recognized in gh-pages-deploy.mjs but the lint script doesn't like it. prolly rempve the lint call from the build script & consider not making any of those build script changes
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
--- vue3-upgrade-test/scripts/gh-pages-deploy.mjs
+++ vue3-upgrade-test/scripts/gh-pages-deploy.mjs
@@ -1,33 +1,36 @@
 import { execa } from 'execa'
-// import { execa } from "/node_modules/execa/index.js"
-import { existsSync } from 'fs'
 
 ;(async () => {
   try {
     // make sure there are no uncommitted changes
     await execa('git', ['update-index', '--refresh'])
 
+    // create empty gh-pages branch that is not based on any previous commit
     await execa('git', ['checkout', '--orphan', 'gh-pages'])
+
+    // build project and commit resulting build files
     console.log('Building...')
     await execa('npm', ['run', 'build'])
-    // Understand if it's dist or build folder
-    const folderName = existsSync('dist') ? 'dist' : 'build'
+    const folderName = 'dist'
     await execa('git', ['--work-tree', folderName, 'add', '--all'])
     await execa('git', ['--work-tree', folderName, 'commit', '-m', 'gh-pages'])
+
+    // push committed files to origin/gh-pages
     console.log('Pushing to gh-pages...')
     await execa('git', ['push', 'origin', 'HEAD:gh-pages', '--force'])
+
+    // clean up build files and local gh-pages branch
     console.log('Cleaning up...')
     await execa('rm', ['-r', folderName])
     await execa('git', ['checkout', '-f', 'main'])
     await execa('git', ['branch', '-D', 'gh-pages'])
+
     console.log('Successfully deployed')
   } catch (e) {
     if (e.cmd === 'git update-index --refresh') {
       console.log('Please stash or commit changes first!')
     }
     console.log('Error: ' + e.message)
-    console.log('Full error:')
-    console.log(e)
     // eslint-disable-next-line no-undef
     process.exit(1)
   }
